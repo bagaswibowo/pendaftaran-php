99f3f0898229b7aa2842e40bac58de2c3b90d342
bagaswibowo <bagaswibowo121@gmail.com>
2019-07-21 22:09:06 +0700

    pendaftaran_siswa

--- config.php
+++ config.php
@@ -1,16 +1,16 @@
 <?php
 
-$host = 'registerdbmysql.mysql.database.azure.com';
-$username = 'bagaswibowo@registerdbmysql';
-$password = 'Advan111';
-$db_name = 'pendaftaran_siswa';
+// $host = 'registerdbmysql.mysql.database.azure.com';
+// $username = 'bagaswibowo@registerdbmysql';
+// $password = 'Advan111';
+// $db_name = 'pendaftaran_siswa';
 
 $conn = mysqli_init();
 mysqli_ssl_set($conn,NULL,NULL, "C:/ssl/BaltimoreCyberTrustRoot.crt.pem", NULL, NULL); 
 
-$db = mysqli_real_connect($host, $username, $password, $db_name, 3306, MYSQLI_CLIENT_SSL, MYSQLI_CLIENT_SSL_DONT_VERIFY_SERVER_CERT);
+$db = mysqli_real_connect($conn, 'registerdbmysql.mysql.database.azure.com', 'bagaswibowo@registerdbmysql', 'Advan111', 'pendaftaran_siswa', 3306, MYSQLI_CLIENT_SSL, MYSQLI_CLIENT_SSL_DONT_VERIFY_SERVER_CERT);
 // ll
-if( !$db ){
-    die("Gagal terhubung dengan database: " . mysqli_connect_error());
-}
+// if( !$db ){
+//     die("Gagal terhubung dengan database: " . mysqli_connect_error());
+// }
 ?>
