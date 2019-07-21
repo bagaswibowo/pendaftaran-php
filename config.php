60c8b10bc0ee702e9202cc66107ed31e054b0a47
bagaswibowo <bagaswibowo121@gmail.com>
2019-07-21 21:53:49 +0700

    host

--- config.php
+++ config.php
@@ -7,6 +7,6 @@ $db_name = 'pendaftaran_siswa';
 
 $conn = mysqli_init();
 mysqli_ssl_set($conn,NULL,NULL, "C:/ssl/BaltimoreCyberTrustRoot.crt.pem", NULL, NULL); 
-$db = mysqli_connect($server, $user, $password, $nama_database, 3306, MYSQLI_CLIENT_SSL, MYSQLI_CLIENT_SSL_DONT_VERIFY_SERVER_CERT);
+$db = mysqli_connect($host, $username, $password, $db_name, 3306, MYSQLI_CLIENT_SSL, MYSQLI_CLIENT_SSL_DONT_VERIFY_SERVER_CERT);
 // ll
 ?>
