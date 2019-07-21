231e28bd50590f026d8ae303a1bf6afc1a00a3b0
bagaswibowo <bagaswibowo121@gmail.com>
2019-07-21 22:01:09 +0700

    gagal

--- config.php
+++ config.php
@@ -9,4 +9,7 @@ $conn = mysqli_init();
 mysqli_ssl_set($conn,NULL,NULL, "C:/ssl/BaltimoreCyberTrustRoot.crt.pem", NULL, NULL); 
 $db = mysqli_connect($host, $username, $password, $db_name, 3306, MYSQLI_CLIENT_SSL, MYSQLI_CLIENT_SSL_DONT_VERIFY_SERVER_CERT);
 // ll
+if( !$db ){
+    die("Gagal terhubung dengan database: " . mysqli_connect_error());
+}
 ?>
