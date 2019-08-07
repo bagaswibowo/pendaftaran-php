73f2e5e6c007cb4434381a5ec2882dee13596787
bagaswibowo <bagaswibowo121@gmail.com>
2019-08-07 17:35:35 +0700

    dbbagaswibowo.mysql.database.azure.com'

--- config.php
+++ config.php
@@ -8,7 +8,7 @@
 $conn = mysqli_init();
 mysqli_ssl_set($conn,NULL,NULL, "C:/ssl/BaltimoreCyberTrustRoot.crt.pem", NULL, NULL); 
 
-mysqli_real_connect($conn, 'registerdbmysql.mysql.database.azure.com', 'bagaswibowo@registerdbmysql', 'Advan111', 'pendaftaran_siswa', 3306, MYSQLI_CLIENT_SSL, MYSQLI_CLIENT_SSL_DONT_VERIFY_SERVER_CERT);
+mysqli_real_connect($conn, 'dbbagaswibowo.mysql.database.azure.com', 'bagaswibowo@dbbagaswibowo', 'Advan111', 'pendaftaran_siswa', 3306, MYSQLI_CLIENT_SSL, MYSQLI_CLIENT_SSL_DONT_VERIFY_SERVER_CERT);
 
 if (mysqli_connect_errno($conn)) {
         die('Failed to connect to MySQL: '.mysqli_connect_error());
